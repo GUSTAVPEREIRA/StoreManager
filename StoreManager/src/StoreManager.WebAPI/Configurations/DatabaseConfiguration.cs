f75a46028c95e82036e94fda9a4791d551afec6c
Lucas Nobrega <lucas.sobrinho@objective.com.br>
2021-08-02 16:54:02 -0300

    merge resolvendo pipino

--- StoreManager/src/StoreManager.WebAPI/Configurations/DatabaseConfiguration.cs
+++ StoreManager/src/StoreManager.WebAPI/Configurations/DatabaseConfiguration.cs
@@ -1,24 +1,7 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using StoreManager.Infrastructure.Context;
-
 namespace StoreManager.WebAPI.Configurations
 {
-    public static class DatabaseConfiguration
+    public class DatabaseConfiguration
     {
-        public static void AddDatabaseConfiguration(this IServiceCollection services, IConfiguration configuration)
-        {
-            services.AddDbContext<StoreContext>(options => options.UseSqlServer(configuration.GetConnectionString("StoreManagerDB")));
-        }
-
-        public static void UseDatabaseConfiguration(this IApplicationBuilder app)
-        {
-            using var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope();
-            using var context = serviceScope.ServiceProvider.GetService<StoreContext>();
-            context.Database.Migrate();
-            context.Database.EnsureCreated();
-        }
+        
     }
 }
